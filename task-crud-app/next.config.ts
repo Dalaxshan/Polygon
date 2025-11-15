8ff252e52936a9e4e563438c56f15da171fec82b
ujanth <80120140+Dalaxshan@users.noreply.github.com>
2025-11-15 19:57:06 +0530

    install next js packages

--- task-crud-app/next.config.ts
+++ task-crud-app/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: '.', // Force root to current folder
-  },
 };
 
 export default nextConfig;
